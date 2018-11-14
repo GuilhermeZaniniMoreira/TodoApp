f2f4e97eb89884c273da34e44ee160bba44060e5
Guilherme Zanini Moreira <guilherme.zm@outlook.com>
2018-11-14 19:06:00 +0000

    Criando schema

---
+++ backend/src/api/todo/todoService.js
@@ -0,0 +1,6 @@
+const Todo = require('../todo')
+
+Todo.methods(['get', 'post', 'put', 'delete'])
+Todo.updateOptions({new: true, runValidators: true}) // quando mandar atualização já retorne registro atualizado
+
+module.exports = Todo
