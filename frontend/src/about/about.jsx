888b62b87cf764271cf5bed9f58e82e735f409f0
Guilherme Zanini Moreira <guilherme.zm@outlook.com>
2018-11-21 18:16:31 +0000

    PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import PageHeader from '../template/pageheader'
 
-export default propts => (
+export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices ex sit amet nisi finibus, nec ultricies lorem lacinia. Fusce congue sagittis feugiat. </p>
     </div>
 )
